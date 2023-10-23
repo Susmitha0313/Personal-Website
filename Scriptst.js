363c5cb9a93713088d51c5eb8b2fe6ee3e708534
Susmitha S <147804718+Susmitha0313@users.noreply.github.com>
2023-10-23 12:38:52 +0530

    Add files via upload

---
+++ Scriptst.js
@@ -0,0 +1,14 @@
+<script>
+
+    function seterror(id, error){
+
+    }
+    function validateform(){
+var name =  document.forms['myform']["fname"].value;
+console.log(name);
+
+
+
+return returnvalue;
+    }
+</script>
